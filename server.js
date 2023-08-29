69141e4f6e627b2e9b03c9cf8b87361b84fcf7b1
Fabio Silveira <fabiosilveira@Fabios-MacBook-Pro.local>
2023-08-28 22:09:01 -0700

    Added Routes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require["express"];
-const htmlRoutes = require("./routes/html-routes");
-const apiRoutes = require("./routes/api-routes");
+const express = require("express");
+const htmlRoutes = require("./routes/html")
+const apiRoutes = require("./routes/api")
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+// Express middleware 
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(htmlRoutes);
-app.use(apiRoutes);
+app.use(htmlRoutes)
+app.use(apiRoutes)
 
-app.listen(PORT, () => (
-    console.log(`Server running on http://localhost:${PORT}`)
-));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
